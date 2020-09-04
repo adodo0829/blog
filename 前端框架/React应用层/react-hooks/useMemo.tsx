11c610a5431ff2f428ec2ed81ae3fb820494be58
appleguardu <appleguardu@163.com>
2020-09-04 18:10:23 +0800

    add useMemo

--- 前端框架/React应用层/react-hooks/useMemo.tsx
+++ 前端框架/React应用层/react-hooks/useMemo.tsx
@@ -5,7 +5,7 @@
 // 因此需要做一些优化
 
 // useMemo: 接受一个函数, 和一个依赖值 => 函数中对依赖值处理后的返回值
-import React, { memo, useState, useMemo } from "react"
+import React, { memo, useState, useMemo } from "react";
 
 const FC = () => {
   const [value, setValue] = useState(0);
@@ -14,11 +14,39 @@ const FC = () => {
     if (value > 2) return value + 1; // 值大于2 increase才有值
   }, [value]);
 
-  return increase
-}
+  return increase;
+};
 
 // memo: 判断一个函数组件的渲染是否重复执行
 const Child = memo(function Child(props) {
-  console.log('Child render') // 只有当props发生变化时, Child才会渲染
+  console.log("Child render"); // 只有当props发生变化时, Child才会渲染
   return <h1>value: {props.value}</h1>;
-})
+});
+
+// 例子: 当操作逻辑导致 状态更新, 组件内部逻辑重新执行; 我们不希望重新执行某些逻辑
+const [detailVisible, setDetailVisible] = useState(false);
+
+const tableConfig = useMemo(() => {
+
+  const operationColumns = [
+    {
+      title: "入库单号",
+      dataIndex: "taskCode",
+      width: 120,
+      fixed: "left",
+      render: (v: string, rowObj: any) => {
+        return (
+          <div
+            style={{ cursor: "pointer" }}
+            onClick={() => setDetailVisible(true)}
+          >
+            {v}
+          </div>
+        );
+      },
+    },
+  ];
+  const newColumn = [...operationColumns, ...Config.columns];
+
+  return { ...Config.tableConfig, columns: newColumn }; // 返回一个缓存值
+}, [setDetailVisible]);
