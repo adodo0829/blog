c97a0d06a9247bed39f5936955b1f5218753bba2
appleguardu <appleguardu@163.com>
2020-10-22 13:43:07 +0800

    add: 多态

--- 设计模式/1.js语言.js
+++ 设计模式/1.js语言.js
@@ -13,13 +13,11 @@ hasValue = [1, 2, 3]
  * 怎么做: 
  *      数据封装: js的变量作用域来模拟公共和私有属性
  *      行为封装: 对象暴露出API的方式
- * 
  */
 
 const Model = (function () {
   // 私有数据
   const name = 'apple'
-
   return {
     getName: () => name // 对外通过API暴露
   }
@@ -30,6 +28,39 @@ console.log(Model.getName())
 /**
  * 2.多态
  * 目的: 把“做什么”和“谁去做”分离开; 去除条件分支; 消除类型间的耦合关系
- * 怎么做: 分离去耦合
- *        
+ * 怎么做: 分离去耦合,
+ *        将过程化的条件分支语句转化为对象的多态性, 
+ *        将行为分布在各个对象中，并让这些对象各自负责自己的行为
  */
+const googleMap = {
+  show: function(){
+    console.log( '开始渲染谷歌地图' );
+  }
+};
+const baiduMap = {
+  show: function(){
+    console.log( '开始渲染百度地图' );
+  }
+};
+const sosoMap = {
+  show: function(){
+    console.log( '开始渲染搜搜地图' );
+  }
+};
+
+const renderMap = (map) => {
+  if (map.show instanceof Function) {
+    map.show()
+  }
+}
+
+renderMap(googleMap)
+
+// 这种方式需要多维护一个判断语句
+const renderMap2 = function( type ){
+  if ( type === 'google' ){
+    googleMap.show();
+  }else if ( type === 'baidu' ){
+    baiduMap.show();
+  }
+};
